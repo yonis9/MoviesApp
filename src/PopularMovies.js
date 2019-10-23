ce6f485b436ca5b356b52612de53d65dc2591b11
Yoni Sisso <yonis9@walla.com>
2019-10-23 18:11:45 +0300

    added loading icon

--- src/PopularMovies.js
+++ src/PopularMovies.js
@@ -6,7 +6,13 @@ const PopularMovies = ( { popularMovies }) => {
     return (
       <div>
         <h3 className='section-header'>Popular Movies</h3>
+        {!popularMovies.length ?
+       <div className='lds-con'>
+       <div className="lds-css ng-scope" style={{width: '200px', height: '200px'}}>
+      <div className="lds-spinner" style={{width:'100%',height:'100%'}}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div></div>
+       :
         <Cardlist  data={popularMovies} />
+        }
         <Footer />
       </div>
     )
--- src/UpcomingMovies.js
+++ src/UpcomingMovies.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Cardlist from './Cardlist';
-import Footer from './Footer'
+import Footer from './Footer';
+
 
 const UpcomingMovies = ({ upcomingMovies }) => {
     return (
+      
      <div>
        <h3 className='section-header'>Upcoming Movies</h3>
+       {!upcomingMovies.length ?
+       <div className='lds-con'>
+       <div className="lds-css ng-scope" style={{width: '200px', height: '200px'}}>
+      <div className="lds-spinner" style={{width:'100%',height:'100%'}}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div></div>
+       :
        <Cardlist  data={upcomingMovies} />
+       }
        <Footer />
      </div>
     )
