ff4c832fc3e6088f7b123d5b87316fda5a00ed30
Yoni Sisso <yonis9@walla.com>
2019-10-23 17:36:13 +0300

    Updates

---
+++ precache-manifest.557ddb4c671884c7c363ce7f7e96786c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90dd560b236a81bf763a35c56a713e2",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "cd42992ed8514df2da19",
+    "url": "/MoviesApp/static/css/main.de1c5878.chunk.css"
+  },
+  {
+    "revision": "51991b28bd6e1ed5e1ff",
+    "url": "/MoviesApp/static/js/2.f05ed739.chunk.js"
+  },
+  {
+    "revision": "cd42992ed8514df2da19",
+    "url": "/MoviesApp/static/js/main.3c5e8084.chunk.js"
+  },
+  {
+    "revision": "59f2dcc43d020b895a37",
+    "url": "/MoviesApp/static/js/runtime-main.6a26f3ce.js"
+  }
+]);
